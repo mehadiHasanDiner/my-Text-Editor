b01f2e0dda5a55f175a6d2262dd915d26bf35372
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-16 02:19:17 +0600

    Bold button added

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,10 @@
-document.getElementById("bold-button").addEventListener("click", function () {
+const boldButtonClicked = document.getElementById("bold-button");
+boldButtonClicked.addEventListener("click", function (e) {
   const textBold = document.getElementById("textarea-field");
-  textBold.style.fontWeight = "bold";
+  if (boldButtonClicked !== e.target) {
+    console.log(e);
+    textBold.style.textDecoration = "none";
+  } else {
+    textBold.style.fontWeight = "bold";
+  }
 });
