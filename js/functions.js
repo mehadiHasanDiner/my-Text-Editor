d837a8f6a481dc565c5650f1ff7d880613eb5ad4
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-18 17:26:50 +0600

    functionality added

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,29 @@
-const boldButtonClicked = document.getElementById("bold-button");
-boldButtonClicked.addEventListener("click", function (e) {
-  const textBold = document.getElementById("textarea-field");
-  if (boldButtonClicked !== e.target) {
-    console.log(e);
-    textBold.style.textDecoration = "none";
+// const boldButtonClicked = document.getElementById("bold-button");
+// const textArea = document.getElementById("textarea-field");
+
+// boldButtonClicked.addEventListener("click", function (e) {
+//   let isClicked = e.target;
+//   if (isClicked === true) {
+//     textArea.classList.add("click-active");
+//   } else {
+//     textArea.classList.remove("click-inactive");
+//   }
+// });
+
+// function checkClicked(isClicked) {}
+
+// if (boldButtonClicked !== e.target) {
+//   console.log(e.target);
+//   textArea.style.textDecoration = "none";
+// } else {
+//   textArea.style.fontWeight = "bold";
+// }
+
+const textArea = document.getElementById("textarea-field");
+function fontBold() {
+  if (textArea.style.fontWeight === "bold") {
+    textArea.style.fontWeight = "normal";
   } else {
-    textBold.style.fontWeight = "bold";
+    textArea.style.fontWeight = "bold";
   }
-});
+}
